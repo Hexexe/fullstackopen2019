94581d688e2d7580f7ddc4e322ad89d54192d13f
Hexexe <TusernameAken@gmail.com>
2019-12-18 22:09:34 +0200

    done ?

--- Osa7/Blog/Front/src/components/User.js
+++ Osa7/Blog/Front/src/components/User.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const User = ({ user }) => {
+	if (!user) {
+		return null
+	}
 	console.log(user)
 	return (
 		<div>
+			<br />
 			<h1>{user.name}</h1>
+			<br />
+			<h3>Added blogs:</h3>
+			<ul>
+				{user.blogs.map(blog => (
+					<li key={blog.id}>{blog.title}</li>
+				))}
+			</ul>
 		</div>
 	)
 }
